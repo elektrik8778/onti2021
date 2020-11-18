3812e674aa837f7642a858e30124f8860a91376a
elektrik8778 <34906459+elektrik8778@users.noreply.github.com>
2020-11-18 21:27:49 +0900

    Update flyght.py

--- flyght.py
+++ flyght.py
@@ -1,104 +1,90 @@
 from __future__ import print_function
-# from std_msgs.msg import String
-from std_msgs.msg import Float32MultiArray
-from sensor_msgs.msg import Image
 import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge 
 from clover import srv
 from std_srvs.srv import Trigger
-import math
-import sys
-import threading
-import os
+from std_msgs.msg import Float32MultiArray
 
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
-import numpy as np
-from threading import Thread
+import math
+from pyzbar.pyzbar import decode
+from PIL import Image as pimg
+# ------------------------------------------------------------------------------------------------------------------------------
 
 rospy.init_node('flight')
+bridge = CvBridge()
+out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10, (320, 240))
+
+def image_call(data):
+    telem = get_telemetry(frame_id='aruco_map')
+    cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
+    cv2.putText(cv_image, 'x = '+str("%.2f" % telem.x), (0,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0),1)
+    cv2.putText(cv_image, 'y = '+str("%.2f" % telem.y), (0,27), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),1)
+    cv2.putText(cv_image, 'z = '+str("%.2f" % telem.z), (0,42), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
+    out.write(cv_image)
+    cv2.imshow('politeh', cv_image)
+    cv2.waitKey(1)
+
+
+def qr_read():
+
+    img = bridge.imgmsg_to_cv2(rospy.wait_for_message('main_camera/image_raw', Image), 'bgr8')
+    cv2.imwrite('qr.jpg', img)
+    barcode = decode(pimg.open('qr.jpg'))
+    print(barcode[0].data)
+# ------------------------------------------------------------------------------------------------------------------------------
+
 
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
 
-class VideoRecorder:
-    def __init__(self):
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        if not os.path.exists(os.environ['HOME']+"/L22_AERO_LOG"):
-            os.mkdir(os.environ['HOME']+"/L22_AERO_LOG")
-        self.UPDATE_RATE = 5
-        self.video_writer = cv2.VideoWriter(os.environ['HOME']+"/L22_AERO_LOG/LOG_IMAGE_RAW_real_drone.avi", self.fourcc, self.UPDATE_RATE, (320, 240))
-        self.image_raw_frame = np.zeros((240, 320, 3), dtype="uint8")
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/main_camera/image_raw_throttled", Image, self.img_clb)
-        self.stopped = False
-    
-    def img_clb(self, msg):
-        self.image_raw_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-
-    def start(self):    
-        Thread(target=self.videowriter, args=()).start()
-        return self
-
-    def videowriter(self):
-        try:
-            r = rospy.Rate(self.UPDATE_RATE)
-            while not self.stopped:
-                self.video_writer.write(self.image_raw_frame)
-                r.sleep()
-        except KeyboardInterrupt:
-            self.video_writer.release()
-            self.stopped = True
-    def stop(self):
-        self.stopped = True
-        self.video_writer.release()
 
 
 
+coords = {1: [0, 0, 1],
+          3: [2, 2, 1],
+          5: [0, 8, 1],
+          7: [2, 8, 1],
+          6: [8, 5, 1],
+          4: [10, 0, 1],
+          2: [10, 10, 1]}
 
+path = [3,4,5]
 
+sensor_value = []
+# ------------------------------------------------------------------------------------------------------------------------------
 
-flag=False
-l = []
-def foo_cal(data):
-    global flag
-    print(data.data[0])
-    l.append(data.data[0])
-    flag=True
-
-
-def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
-    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
-
-    while not rospy.is_shutdown():
-        telem = get_telemetry(frame_id='navigate_target')
-        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
-            break
-        rospy.sleep(0.2)
-
-# navigate_wait(x=0, y=0, z=1, speed=0.5, frame_id='body', auto_arm=True)
-
-vr = VideoRecorder().start()
-
-coords = {1:[10,10,1],
-          2:[0,10,1],
-          3:[10,0,1],
-          4:[5,5,1],
-          5:[8,5,1],
-          6:[5,8,1]}
-
-path = [1,2]
-
+image_sub = rospy.Subscriber('/main_camera/image_raw_throttled', Image, image_call)
+navigate(x=0, y=0, z=1, speed=1, frame_id='body', auto_arm=True)
+rospy.sleep(2)
+navigate(x=0, y=0, z=1, speed=1, frame_id='aruco_map')
+rospy.sleep(1)
 for i in path:
-    navigate_wait(x=coords[i][0], y=coords[i][1], z=coords[i][2], speed=1, frame_id='aruco_map')
-    r = rospy.Subscriber('sensors', Float32MultiArray, foo_cal)
-    while not flag:
-        rospy.sleep(0.05)
-    r.unregister()
-    flag = False
+    print("go to ", i, coords[i])
+    navigate(x = coords[i][0], y= coords[i][1], z = coords[i][2], speed = 2.5, frame_id = 'aruco_map')
+    if i==4:
+        rospy.sleep(8)
+        print("read qr_code...")
+        navigate(x = coords[i][0], y= coords[i][1], z = coords[i][2]-0.5, speed = 0.5, frame_id = 'aruco_map')
+        rospy.sleep(5)
+        try:
+            qr_read()
+        except:
+            print('not read qr')
+        continue
+    rospy.sleep(8)
+    try:
+        s = rospy.wait_for_message('sensors',Float32MultiArray).data[0]
+        sensor_value.append(s)
+    except:
+        print('--')
+navigate(x=0, y=0, z=1, speed=2, frame_id='aruco_map')
+rospy.sleep(7)
+land()
+rospy.sleep(2)
 
-print(l)
-navigate_wait(x=0, y=0, z=1, speed=1, frame_id='aruco_map')
-vr.stop()
-
-# land()
+print(sensor_value)
+out.release()
+cv2.destroyAllWindows()
