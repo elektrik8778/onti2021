67abc80488cfa34509e7c819a72dd5ce4d931641
Vladserdyk <74760286+Vladserdyk@users.noreply.github.com>
2020-11-22 06:41:16 +0300

    Update Vlad.py

--- Vlad.py
+++ Vlad.py
@@ -13,12 +13,12 @@ out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10,
 def image_callback(data):
     telem = get_telemetry(frame_id='aruco_map')
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
-    cv2.putText(cv_image, 'y = '+str("%.1f" % telem.y), (0,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0),1)
-    cv2.putText(cv_image, 'x = '+str("%.1f" % telem.x), (0,24), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),1)
-    cv2.putText(cv_image, 'z = '+str("%.1f" % telem.z), (0,36), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
+    cv2.putText(cv_image, 'y = '+str("%.2f" % telem.y), (0,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0),1)
+    cv2.putText(cv_image, 'x = '+str("%.2f" % telem.x), (0,24), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),1)
+    cv2.putText(cv_image, 'z = '+str("%.2f" % telem.z), (0,36), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1)
     out.write(cv_image)
     
-    cv2.imshow('vot ono politeh', cv_image)
+    cv2.imshow('nti', cv_image)
     cv2.waitKey(1)
 
 # rospy.wait_for_message('')
@@ -28,13 +28,7 @@ navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
 
 image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback)
-navigate(x=0, y=0, z=1, speed=1, frame_id='body', auto_arm=True)
-rospy.sleep(10)
-navigate(x=0, y=0, z=1, speed=1, frame_id='aruco_map')
-rospy.sleep(1)
 
-navigate(x=4, y=3, z=1, speed=1, frame_id='aruco_map')
-rospy.sleep(8)
 
 land()
 rospy.sleep(3)
