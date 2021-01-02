833a0f42cd5dcc400a65855a4347467423716500
elektrik8778 <34906459+elektrik8778@users.noreply.github.com>
2021-01-02 16:07:06 +0900

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+git clone https://github.com/elektrik8778/onti2021.git
+cd stainer
+
+
+cp -f Vlad.py /home/clover/Desktop/stainer/2/
+
+cp -f cheat_list.txt /home/clover/Desktop/stainer/1/
+
+cd ..
+rm -rf onti2021
+
+echo "Done!"
